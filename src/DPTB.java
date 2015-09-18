c9ea7f9e4e7d863f6bf30330581ac87885545735
Legoman6157 <joabyers@comcast.net>
2015-09-18 15:31:36 -0400

    CODE!?!?!?
    Yes.

---
+++ src/DPTB.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class DPTB {
+		
+	public static void main(String[] args) {
+		JFrame
+			window = new JFrame("Don't Push That Button!");
+		JButton
+			button = new JButton("Button");
+		
+		window.add(button);
+		window.setSize(150, 50);
+		window.setVisible(true);
+		window.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		button.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				DPTB.main(args);
+			}
+		});
+		
+	}//main
+}//DPTB
