b0171e93cc6200f2c73663195e8b77adb1700fa1
Legoman6157 <joabyers@comcast.net>
2015-10-01 22:01:19 -0400

    Commenting
    /* //Commenting */

--- src/DPTB.java
+++ src/DPTB.java
@@ -1,3 +1,15 @@
+// DPTB.java Version 2
+// ** WARNING: DO NOT CREATE TOO MANY WINDOWS, OR ELSE YOU MAY HARM YOUR COMPUTER **
+// Joshua Byers
+// Last Edited on September 25, 2015 around 5:31
+// This program recreates its window each time you press 'button'.
+//
+// Version 1:
+//		The button.
+// Version 2:
+//		Quit button.
+
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -18,13 +30,17 @@ public class DPTB {
 		window.setVisible(true);
 		window.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
+		//When you push 'button':
 		button.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
+				//It creates a new window.
 				DPTB.main(args);
 			}
 		});
+		
+		//Quits the program
 		quit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
